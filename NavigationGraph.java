e9735626ecfc0459b268e20c7cb4f021c2f01184
rom4ster <rohansampat97@gmail.com>
2017-04-23 15:24:18 -0500

    Proper commit
    -_-

--- NavigationGraph.java
+++ NavigationGraph.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class NavigationGraph implements GraphADT<Location, Path> {
 
 	//TODO: Implement all methods of GraphADT
@@ -16,5 +18,61 @@ public class NavigationGraph implements GraphADT<Location, Path> {
 	public Location getLocationByName(String name) {
 		return null; //TODO: implement correctly. 
 	}
+
+
+    @Override
+    public void addVertex(Location vertex) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void addEdge(Location src, Location dest, Path edge) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public List<Location> getVertices() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public Path getEdgeIfExists(Location src, Location dest) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public List<Path> getOutEdges(Location src) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public List<Location> getNeighbors(Location vertex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public List<Path> getShortestRoute(Location src, Location dest, String edgePropertyName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public String[] getEdgePropertyNames() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
