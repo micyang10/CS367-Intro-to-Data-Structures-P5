3a65bc0d97cdebb861fd83524e19effc441a8e90
micyang10 <yang363@wisc.edu>
2017-05-04 21:50:16 -0500

    Update MapApp.java

--- MapApp.java
+++ MapApp.java
@@ -1,3 +1,15 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017 
+// PROJECT:          Program 5
+// FILE:             NavigationGraph.java
+//
+// TEAM:    P5 Team 102
+// Authors: Michael Yang, Rohan Sampat
+// Author1: Michael Yang, yang363@wisc.edu, yang363, LEC 001
+// Author2: Rohan Sampat, rsampat@wisc.edu, rsampat, LEC 001
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -9,11 +21,12 @@ import java.util.Scanner;
  * Driver class that reads/parses the input file and creates NavigationGraph
  * object.
  * 
- * @author CS367
- *
+ * * <p>Bugs: None that we are aware of
+ * 
+ * @author CS367, Michael Yang, Rohan Sampat
  */
 public class MapApp {
-
+	//a graph object 
 	private NavigationGraph graphObject;
 
 	/**
@@ -26,7 +39,14 @@ public class MapApp {
 		this.graphObject = graph;
 	}
 
-	
+	/**
+	 * Main function which takes the command line arguments and 
+	 * instantiate the MapApp class.
+	 * The main function terminates depending on user input
+	 * Use the startService() method to read inputs from console
+	 * 
+	 * @param args Command line arguments of <directory> 
+	 */
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.out.println("Usage: java MapApp <pathToGraphFile>");
@@ -36,25 +56,25 @@ public class MapApp {
 		// read the filename from command line argument
 		String locationFileName = args[0];
 		try {
-			NavigationGraph graph = createNavigationGraphFromMapFile(locationFileName);
+			NavigationGraph graph = createNavigationGraphFromMapFile
+					(locationFileName);
 			MapApp appInstance = new MapApp(graph);
 			appInstance.startService();
 
 		} catch (FileNotFoundException e) {
-			System.out.println("GRAPH FILE: " + locationFileName + " was not found.");
+			System.out.println("GRAPH FILE: " + locationFileName 
+					+ " was not found.");
 			System.exit(1);
 		} catch (InvalidFileException e) {
 			System.out.println(e.getMessage());
 			System.exit(1);
 		}
-
 	}
 
 	/**
-	 * Displays options to user about the various operations on the loaded graph
+	 * Displays options to user about the various operations on loaded graph
 	 */
 	public void startService() {
-
 		System.out.println("Navigation App");
 		Scanner sc = new Scanner(System.in);
 
@@ -109,12 +129,14 @@ public class MapApp {
 				Location dest = graphObject.getLocationByName(destName);
 
 				if (src == null || dest == null) {
-					System.out.println(srcName + " and/or " + destName + " are not valid Locations in the graph");
+					System.out.println(srcName + " and/or " + destName 
+							+ " are not valid Locations in the graph");
 					break;
 				}
 
 				if (src == dest) {
-					System.out.println(srcName + " and " + destName + " correspond to the same Location");
+					System.out.println(srcName + " and " + destName 
+							+ " correspond to the same Location");
 					break;
 				}
 
@@ -122,20 +144,26 @@ public class MapApp {
 				// List Edge Property Names
 				String[] propertyNames = graphObject.getEdgePropertyNames();
 				for (int i = 0; i < propertyNames.length; i++) {
-					System.out.println("\t" + (i + 1) + ": " + propertyNames[i]);
+					System.out.println("\t" + (i + 1) + ": " 
+							+ propertyNames[i]);
 				}
-				System.out.println("Select property to compute shortest route on: ");
+				System.out.println("Select property to compute" 
+							+ " shortest route on: ");
 				int selectedPropertyIndex = sc.nextInt() - 1;
 
 				if (selectedPropertyIndex >= propertyNames.length) {
-					System.out.println("Invalid option chosen: " + (selectedPropertyIndex + 1));
+					System.out.println("Invalid option chosen: " 
+								+ (selectedPropertyIndex + 1));
 					break;
 				}
 
-				String selectedPropertyName = propertyNames[selectedPropertyIndex];
-				List<Path> shortestRoute = graphObject.getShortestRoute(src, dest, selectedPropertyName);
+				String selectedPropertyName 
+						= propertyNames[selectedPropertyIndex];
+				List<Path> shortestRoute = graphObject.getShortestRoute
+						(src, dest, selectedPropertyName);
 				for(Path path : shortestRoute) {
-					System.out.print(path.displayPathWithProperty(selectedPropertyIndex)+", ");
+					System.out.print(path.displayPathWithProperty
+							(selectedPropertyIndex)+", ");
 				}
 				if(shortestRoute.size()==0) {
 					System.out.print("No route exists");
@@ -150,7 +178,6 @@ public class MapApp {
 			default:
 				System.out.println("Please select a valid option: ");
 				break;
-
 			}
 		} while (choice != 5);
 		sc.close();
@@ -171,12 +198,13 @@ public class MapApp {
 	 *             if graphFilepath is not found
 	 * @throws InvalidFileException
 	 *             if header line in the file has < 3 columns or 
-	 *             if any line that describes an edge has different number of properties 
-	 *             	than as described in the header or 
+	 *             if any line that describes an edge has different 
+	 *             number of properties than as described in the header or 
 	 *             if any property value is not numeric 
 	 */
-
-	public static NavigationGraph createNavigationGraphFromMapFile(String graphFilepath) throws FileNotFoundException, InvalidFileException {
+	public static NavigationGraph createNavigationGraphFromMapFile
+			(String graphFilepath) throws FileNotFoundException, 
+			InvalidFileException {
 			File file = new File(graphFilepath);
 	        Scanner fstream = new Scanner(file);
 			String header = fstream.nextLine();
@@ -185,7 +213,8 @@ public class MapApp {
 			String[] arr = header.split(" ");
 			//System.out.println(arr.length);
 			if (arr.length == 0) {
-			    throw new InvalidFileException("Invalid File: Missing Headers");
+			    throw new InvalidFileException("Invalid File: " 
+			    			+ "Missing Headers");
 			}
 			for (int i = 0; i < arr.length; i++) {
 			   arr[i]= parse(arr[i]);
@@ -196,14 +225,14 @@ public class MapApp {
 	           String fileLine = fstream.nextLine();
 	           String[] lineArr = fileLine.split(" ");
 	           if (lineArr.length != arr.length+2) {
-	               throw new InvalidFileException("Invalid File: Row_" + test + " Incomplete Entry");
+	               throw new InvalidFileException("Invalid File: Row_" 
+	            		   		+ test + " Incomplete Entry");
 	           }
 	           
 	           List<Double> list = new ArrayList<Double>();
 	           for(int i = 0; i < lineArr.length; i++) {
 	               if (i == 1 || i == 0) {
-	                   graph.addVertex(new Location(parse(lineArr[i])));
-	                   
+	                   graph.addVertex(new Location(parse(lineArr[i])));	                   
 	               } else {
 	                   try {
 	                       double temp = Double.parseDouble(lineArr[i]);
@@ -213,26 +242,28 @@ public class MapApp {
 	                           list.add(temp);
 	                       }
 	                   } catch (NumberFormatException e) {
-	                       throw new InvalidFileException("Invalid File: Row_" + test + " " + lineArr[i] + " is not a Double!");
-	                   }
-	                   
-	               }
-	               
-	             
-	               
+	                       throw new InvalidFileException
+	                       				("Invalid File: Row_" + test 
+	                       				+ " " + lineArr[i] 
+	                       				+ " is not a Double!");
+	                   }	                   
+	               }    
 	           }
 	           test++;
 	           Location srcL = graph.getLocationByName(parse(lineArr[0]));
                Location destL = graph.getLocationByName(parse(lineArr[1]));
 	           graph.addEdge(srcL, destL, new Path(srcL, destL, list));
-	        }
-			
+	        }			
 			return graph;
-
 	}
 	
+	/**
+	 * Method that parse and replaces with a certain string
+	 * 
+	 * @param text string to be parsed and replaced
+	 */
 	private static String parse(String text) {
-	    return text.replaceAll("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])", " ").toLowerCase().trim();
+	    return text.replaceAll("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])"
+	    			, " ").toLowerCase().trim();
 	}
-
 }
